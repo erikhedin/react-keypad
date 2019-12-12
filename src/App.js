45ee4a7b4e60b8a1885181b5d1b90e2bcd993e7f
Erik Hedin <erik.hedin@docker.com>
2019-12-12 09:45:03 -0800

    Code formatted with Prettier

--- src/App.js
+++ src/App.js
@@ -1,27 +1,27 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      password: '',
-      lastkey: '',
+      password: "",
+      lastkey: "",
       counter: 0
-    }
+    };
     this.keypad = {
-      a: { num: '1', keys: '' },
-      b: { num: '2', keys: 'ABC' },
-      c: { num: '3', keys: 'DEF' },
-      d: { num: '4', keys: 'GHI' },
-      e: { num: '5', keys: 'JKL' },
-      f: { num: '6', keys: 'MNO' },
-      g: { num: '7', keys: 'PQRS' },
-      h: { num: '8', keys: 'TUV' },
-      i: { num: '9', keys: 'WXYZ' },
-      j: { num: '', keys: '' },
-      k: { num: '0', keys: '' },
-      l: { num: 'X', keys: '' }
+      a: { num: "1", keys: "" },
+      b: { num: "2", keys: "ABC" },
+      c: { num: "3", keys: "DEF" },
+      d: { num: "4", keys: "GHI" },
+      e: { num: "5", keys: "JKL" },
+      f: { num: "6", keys: "MNO" },
+      g: { num: "7", keys: "PQRS" },
+      h: { num: "8", keys: "TUV" },
+      i: { num: "9", keys: "WXYZ" },
+      j: { num: "", keys: "" },
+      k: { num: "0", keys: "" },
+      l: { num: "X", keys: "" }
     };
     this.updatePassword = this.updatePassword.bind(this);
     this.queueKey = this.queueKey.bind(this);
@@ -34,24 +34,21 @@ class App extends React.Component {
     let queue = false;
 
     if (window.timer) clearTimeout(window.timer);
-    if (numberPressed === 'password') return false;
-
-    else if (numberPressed === 'X') {
+    if (numberPressed === "password") return false;
+    else if (numberPressed === "X") {
       password = password.slice(0, -1);
-      lastkey = '';
+      lastkey = "";
       counter = 0;
-    }
-    else {
+    } else {
       let keys = numberPressed + e.target.value;
-      keys = keys.split('');
-      if (keys.length > 1 && keys.length >= (counter + 1)) {
+      keys = keys.split("");
+      if (keys.length > 1 && keys.length >= counter + 1) {
         lastkey = keys[counter];
         counter++;
         queue = true;
-      }
-      else {
+      } else {
         password += numberPressed;
-        lastkey = '';
+        lastkey = "";
         counter = 0;
       }
     }
@@ -63,13 +60,12 @@ class App extends React.Component {
       counter: counter
     });
     if (queue) window.timer = setTimeout(this.queueKey, 300);
-
   }
   queueKey() {
     this.setState({
       password: this.state.password + this.state.lastkey,
-      lastkey: '',
-      counter: 0,
+      lastkey: "",
+      counter: 0
     });
   }
   render() {
@@ -79,16 +75,28 @@ class App extends React.Component {
         <h1>Phone Keypad Password Entry</h1>
         <form>
           <label>Password</label>
-          <input name="password" value={this.state.password} onChange={this.updatePassword}></input>
+          <input
+            name="password"
+            value={this.state.password}
+            onChange={this.updatePassword}
+          ></input>
         </form>
         <div className="keypad">
           {Object.keys(keypad).map((key, i) => (
-            <button key={i} name={keypad[key].num} value={keypad[key].keys} onClick={this.updatePassword}>{keypad[key].num}<br />{keypad[key].keys}</button>
+            <button
+              key={i}
+              name={keypad[key].num}
+              value={keypad[key].keys}
+              onClick={this.updatePassword}
+            >
+              {keypad[key].num}
+              <br />
+              {keypad[key].keys}
+            </button>
           ))}
         </div>
       </div>
     );
-
   }
 }
 export default App;
