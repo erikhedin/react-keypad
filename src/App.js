33675616e83992d45771d9c1fcec671eb50e011f
Erik Hedin <erik.hedin@docker.com>
2020-01-26 20:10:23 +0000

    Update to simplify key object and re-scope the timeout functionality.

--- src/App.js
+++ src/App.js
@@ -9,23 +9,40 @@ class App extends React.Component {
       lastkey: "",
       counter: 0
     };
-    this.keypad = {
-      a: "1",
-      b: "2ABC",
-      c: "3DEF",
-      d: "4GHI",
-      e: "5JKL",
-      f: "6MNO",
-      g: "7PQRS",
-      h: "8TUV",
-      i: "9WXYZ",
-      j: "",
-      k: "0",
-      l: "X"
-    };
+    // this.keypad = {
+    //   a: "1",
+    //   b: "2ABC",
+    //   c: "3DEF",
+    //   d: "4GHI",
+    //   e: "5JKL",
+    //   f: "6MNO",
+    //   g: "7PQRS",
+    //   h: "8TUV",
+    //   i: "9WXYZ",
+    //   j: "",
+    //   k: "0",
+    //   l: "X"
+    // };
+    this.keypad = [
+       "1",
+       "2ABC",
+       "3DEF",
+       "4GHI",
+       "5JKL",
+       "6MNO",
+       "7PQRS",
+       "8TUV",
+       "9WXYZ",
+       "",
+       "0",
+       "X"
+    ];
     this.updatePassword = this.updatePassword.bind(this);
     this.queueKey = this.queueKey.bind(this);
+    let timer = null;
+    this.timer = timer;
   }
+  
   updatePassword(e) {
     let numberPressed = e.target.name;
     let keys = e.target.value.split("");
@@ -34,7 +51,7 @@ class App extends React.Component {
     let counter = this.state.counter;
     let queue = false;
 
-    if (window.timer) clearTimeout(window.timer);
+    if(this.timer) clearTimeout(this.timer);
     if (numberPressed === "password") return false;
     else if (numberPressed === "X") {
       password = password.slice(0, -1);
@@ -47,19 +64,20 @@ class App extends React.Component {
       password = password.slice(0, -1);
       password += keys[counter];
       counter++;
-      queue = true
+      queue = true;
     }
     else {
       password += numberPressed;
       lastkey = numberPressed;
       counter = 0;
+      queue = true;
     }
     this.setState({
       password: password,
       lastkey: lastkey,
       counter: counter
     });
-    if (queue) window.timer = setTimeout(this.queueKey, 500);
+    if (queue) this.timer = setTimeout(this.queueKey, 750);
   }
   queueKey() {
     this.setState({
@@ -69,7 +87,6 @@ class App extends React.Component {
     });
   }
   render() {
-    const keypad = this.keypad;
     let password = this.state.password.slice(0, -1).replace(/./g, '*');
     password += this.state.password.slice(-1);
 
@@ -79,23 +96,22 @@ class App extends React.Component {
         <form>
           <label>Password</label>
           <input
-            type="password"
             name="password"
             value={password}
             onChange={this.updatePassword}
           ></input>
         </form>
         <div className="keypad">
-          {Object.keys(keypad).map((key, i) => (
+          {this.keypad.map((key, i) => (
             <button
               key={i}
-              name={keypad[key][0]}
-              value={keypad[key]}
+              name={key[0]}
+              value={key}
               onClick={this.updatePassword}
             >
-              {keypad[key][0]}
+              {key[0]}
               <br />
-              {keypad[key].slice(1)}
+              {key.slice(1)}
             </button>
           ))}
         </div>
